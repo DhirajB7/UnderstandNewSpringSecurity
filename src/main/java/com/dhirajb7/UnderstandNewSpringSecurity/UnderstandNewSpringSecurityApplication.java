d1e021b1ef693c08af31ba06ac8d67d1ed9426ec
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-05-04 22:35:27 +0530

    first commit

---
+++ src/main/java/com/dhirajb7/UnderstandNewSpringSecurity/UnderstandNewSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.dhirajb7.UnderstandNewSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandNewSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderstandNewSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhirajb7/UnderstandNewSpringSecurity/UnderstandNewSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhirajb7.UnderstandNewSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandNewSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
